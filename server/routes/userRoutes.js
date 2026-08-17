d319da7e3b72a60a65af4b1e8cbf5bc566bdd337
Sahil Kamila <sahilkamila143@gmail.com>
2026-08-17 21:04:22 +0530

    refactor: harden authentication and authorization

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,15 +1,36 @@
 import express from 'express'
-import { addUserRating, getUserCourseProgress, getUserData, purchaseCourse, updateUserCourseProgress, userEnrolledCourses } from '../controllers/userController.js';
+import { 
+    addUserRating, 
+    getUserCourseProgress, 
+    getUserData, 
+    purchaseCourse, 
+    updateUserCourseProgress, 
+    userEnrolledCourses 
+} from '../controllers/userController.js';
+import { protectAuth } from '../middlewares/authMiddleware.js';
 
 
 const userRouter = express.Router()
 
+// All user routes require authentication
+userRouter.use(protectAuth)
+
 // Get user Data
 userRouter.get('/data', getUserData)
+
+// Purchase course
 userRouter.post('/purchase', purchaseCourse)
+
+// Get enrolled courses
 userRouter.get('/enrolled-courses', userEnrolledCourses)
+
+// Update course progress
 userRouter.post('/update-course-progress', updateUserCourseProgress)
+
+// Get course progress
 userRouter.post('/get-course-progress', getUserCourseProgress)
+
+// Add course rating
 userRouter.post('/add-rating', addUserRating)
 
 export default userRouter;
